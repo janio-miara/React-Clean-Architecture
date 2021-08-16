c1a55dac45308f2d53e79d7b7dd82ddd7c0ef071
janio_miara <janio.miara@somoseducacao.com.br>
2021-08-16 18:09:40 -0300

    refactor: move HttoPostClientSpay to ist own file

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,12 @@
 import { RemouteAuthentication } from './remote-authentication'
-import { HttpPostClient } from '../../protocols/http/http-post-client'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
 
 describe('RemouteAuthentication', () => {
   test('Should call HttoClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string;
-      async post (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
     const url = ' any_url'
-    const httpPostClient = new HttpPostClientSpy()
-    const sut = new RemouteAuthentication(url, httpPostClient)
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemouteAuthentication(url, httpPostClientSpy)
     await sut.auth()
-    expect(httpPostClient.url).toBe(url)
+    expect(httpPostClientSpy.url).toBe(url)
   })
 })
