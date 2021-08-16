9ced0791baf72b03fa998369e9ae144ce03658a0
janio_miara <janio.miara@somoseducacao.com.br>
2021-08-16 18:24:47 -0300

    chore: ajustes

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,26 @@
 import { RemouteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
+import * as faker from 'faker'
+
+type SutTypes = {
+  sut: RemouteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemouteAuthentication(url, httpPostClientSpy)
+
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
 
 describe('RemouteAuthentication', () => {
-  test('Should call HttoClient with correct URL', async () => {
-    const url = ' any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemouteAuthentication(url, httpPostClientSpy)
+  test('Should call HttpPostClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,6 +3,6 @@ import { HttpPostClient } from '../../protocols/http/http-post-client'
 export class RemouteAuthentication {
   constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) {}
   async auth (): Promise<void> {
-    await this.httpPostClient.post(this.url)
+    await this.httpPostClient.post({ url: this.url })
   }
 }
